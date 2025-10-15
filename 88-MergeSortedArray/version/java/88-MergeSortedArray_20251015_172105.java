71548caa9f8b8555d02d08b2610b65068f8cdcc2
Raghav Agarwal <144380377+git-raghav@users.noreply.github.com>
2025-10-15 17:21:06 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/version/java/88-MergeSortedArray_20251015_172105.java
@@ -0,0 +1,28 @@
+// Last updated: 10/15/2025, 5:21:05 PM
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int temp[] = new int[m+n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < m && j < n){
+            if(nums1[i] < nums2[j]){
+                temp[k++] = nums1[i++];
+            } else {
+                temp[k++] = nums2[j++];
+            }
+        }
+
+        while(i < m){
+            temp[k++] = nums1[i++];
+        }
+        while(j < n){
+            temp[k++] = nums2[j++];
+        }
+
+        for(k = 0; k < temp.length; k++){
+            nums1[k] = temp[k];
+        }
+    }
+}
