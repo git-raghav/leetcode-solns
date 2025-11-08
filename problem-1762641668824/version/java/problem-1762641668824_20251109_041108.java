e11871d93583823e4167230f47b7db0d18d08daf
Raghav Agarwal <144380377+git-raghav@users.noreply.github.com>
2025-11-09 04:11:09 +0530

    Create

---
+++ problem-1762641668824/version/java/problem-1762641668824_20251109_041108.java
@@ -0,0 +1,18 @@
+// Last updated: 11/9/2025, 4:11:08 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(target < nums[mid]){
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
