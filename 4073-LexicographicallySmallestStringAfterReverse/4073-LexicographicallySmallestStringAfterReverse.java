91ec1766bc1b76769eeca29760ed1ee578791c08
Raghav Agarwal <144380377+git-raghav@users.noreply.github.com>
2026-06-26 00:07:07 +0530

    Create 4073-LexicographicallySmallestStringAfterReverse

---
+++ 4073-LexicographicallySmallestStringAfterReverse/4073-LexicographicallySmallestStringAfterReverse.java
@@ -0,0 +1,20 @@
+// Last updated: 6/26/2026, 12:07:06 AM
+class Solution {
+    public String lexSmallest(String s) {
+        String best = s;
+        int n = s.length();
+
+        for(int k = 1; k <= n; k++){
+            String firstRev = new StringBuilder(s.substring(0, k)).reverse().append(s.substring(k)).toString();
+            if(firstRev.compareTo(best) < 0) {
+                best = firstRev;
+            }
+            String lastRev = s.substring(0, n-k) + new StringBuilder(s.substring(n-k)).reverse().toString();
+            if(lastRev.compareTo(best) < 0) {
+                best = lastRev;
+            }
+        }
+
+        return best;
+    }
+}
