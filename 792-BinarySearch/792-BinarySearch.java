e05f941bd4cf075a6b55eb46db1a0b87ec3ae3fd
Raghav Agarwal <144380377+git-raghav@users.noreply.github.com>
2026-06-26 00:07:41 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,18 @@
+// Last updated: 6/26/2026, 12:07:40 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(target < nums[mid]){
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
