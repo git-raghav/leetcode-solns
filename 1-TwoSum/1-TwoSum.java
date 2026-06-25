621f1dc2ff3035cf2736c09cb0c762b6b85e6dce
Raghav Agarwal <144380377+git-raghav@users.noreply.github.com>
2026-06-26 00:12:33 +0530

    Optimize twoSum method using HashMap

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,13 +1,15 @@
 // Last updated: 8/12/2025, 12:42:27 AM
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i, j};
-                }
+        //target = x+y, x = target-y
+        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target-nums[i];
+            if(numToIndex.containsKey(complement)){
+                return new int[]{numToIndex.get(complement), i};
             }
+            numToIndex.put(nums[i], i);
         }
-        return new int[]{}; // No solution found
+        return new int[]{-1, -1}; // No solution found
     }
 }
